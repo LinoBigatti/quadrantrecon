5efb90c04e8bdd5ed93fe5aff152a0a74ccda160
Lino Bigatti <linobigatti@protonmail.com>
2024-05-19 15:57:07 -0300

    Add proper arg parsing and batch processing.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
@@ -71,43 +72,61 @@ def get_inner_bb(mask):
         prev_had_mask = has_mask
 
     cropped_mask = [list(islice(row, start_x, end_x)) for row in islice(mask, start_y, end_y)]
+    flipped_cropped_mask = np.swapaxes(cropped_mask, 0, 1)
+    
+    bb_x = 0
+    bb_y = 0
+    
+    done = False
+    while not done:
+        done = True
 
-    while sum(chain.from_iterable(cropped_mask)) > 0:
-        print(sum(chain.from_iterable(cropped_mask)))
-        start_x += 1
-        end_x -= 1
+        # Check row
+        if True in cropped_mask[bb_y][bb_x:bb_x+INNER_WIDTH]:
+            bb_y += 1
+            
+            done = False
 
-        start_y += 1
-        end_y -= 1
+        # Check column
+        if True in flipped_cropped_mask[bb_x][bb_y:bb_y+INNER_WIDTH]:
+            bb_x += 1
 
-        cropped_mask = [list(islice(row, start_x, end_x)) for row in islice(mask, start_y, end_y)]
+            done = False
 
-    return [start_x, start_y, end_x, end_y] 
+    x = start_x + bb_x
+    y = start_y + bb_y
 
-def main(filename: str, _plot: bool):
-    _log(f"Loading image from path ${filename}...")
-    image = cv2.imread(filename)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    return [x, y, x + INNER_WIDTH, y + INNER_HEIGHT] 
 
+def main(args: argparse.Namespace):
+    _log("Loading model...")
+    
+    sam = sam_model_registry["default"](checkpoint="./models/sam_vit_h_4b8939.pth")
+    sam.to(device = args.device)
+
+    _log("Model loaded.")
+    
+    _log("Creating predictor...")
+    predictor = SamPredictor(sam)
+
+    for filename in args.filename:
+        process_image(filename, args.plot, predictor)
+
+def process_image(filename: str, _plot: bool, predictor: SamPredictor):
     metadata = pyexiv2.ImageMetadata(filename)
     metadata.read()
 
     if metadata['Exif.Photo.UserComment'].raw_value == "_quadrantrecon_marker":
         print("ERROR: This image has already been modified by quadrantrecon.")
 
-        exit()
+        return
+
+    _log(f"Loading image from path ${filename}...")
+    image = cv2.imread(filename)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
     _log("Image loaded.")
 
-    _log("Loading model...")
-    
-    sam = sam_model_registry["default"](checkpoint="./models/sam_vit_h_4b8939.pth")
-    sam.to(device = "cuda")
-
-    _log("Model loaded.")
-    
-    _log("Creating predictor...")
-    predictor = SamPredictor(sam)
     _log("Setting predictor image...")
     predictor.set_image(image)
     _log("Predictor loaded")
@@ -125,43 +144,25 @@ def main(filename: str, _plot: bool):
         plt.show()
 
     _log("Predicting...")
-    masks, scores, logits = predictor.predict(
+    masks, scores, _ = predictor.predict(
         point_coords=input_points,
         point_labels=input_labels,
         multimask_output=True,
     )
 
-    max_score = -1
-    max_score_mask = None
-
-    _log("Showing predictions")
-    for i, (mask, score) in enumerate(zip(masks, scores)):
-        if score > max_score:
-            max_score_mask = mask
-        
-        if __DEBUG__ and _plot:
-            _log(f"Plotting prediction ${i}...")
-        
-            plt.figure(figsize=(10,10))
-            plt.imshow(image)
-
-            show_mask(mask, plt.gca())
-            show_points(input_points, input_labels, plt.gca())
-
-            plt.title(f"Mask {i+1}, Score: {score:.3f}", fontsize=18)
-            plt.axis("off")
-            plt.show()
-
+    # The last output is the highest scoring one.
+    mask = masks[-1]
+    score = scores[-1]
 
     if _plot:
-        _log("Plotting biggest score prediction...")
+        _log("Plotting prediction...")
 
         plt.figure(figsize=(10, 10))
         plt.imshow(image)
 
-        show_mask(max_score_mask, plt.gca())
+        show_mask(mask, plt.gca())
 
-        plt.title(f"Highest score mask, Score: {max_score:.3f}", fontsize=18)
+        plt.title(f"Predicted mask, Score: {score:.3f}", fontsize=18)
         plt.axis("off")
         plt.show()
     
@@ -170,12 +171,12 @@ def main(filename: str, _plot: bool):
     bb = get_inner_bb(mask)
 
     if _plot:
-        _log("Plotting inner mask for biggest score prediction...")
+        _log("Plotting inner bounding box for predicted mask...")
 
         plt.figure(figsize=(10, 10))
         plt.imshow(image)
 
-        show_mask(max_score_mask, plt.gca())
+        show_mask(mask, plt.gca())
 
         show_box(bb, plt.gca())
 
@@ -218,15 +219,42 @@ def main(filename: str, _plot: bool):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Error: Please use a path.")
+    parser = argparse.ArgumentParser(
+        prog="QuadrantRecon",
+        description="Finds and crops out quadrants in images",
+        epilog="(c) Lino Bigatti"
+    )
 
-        exit()
+    parser.add_argument("filename",
+                        help="one of the images to crop",
+                        nargs="+")
+    parser.add_argument("-v", "--verbose",
+                        help="display debug information",
+                        action="store_true")
+    parser.add_argument("-p", "--plot",
+                        help="plot results while working",
+                        action="store_true")
+    parser.add_argument("-d", "--device",
+                        help="device to run on (default: %(default)s)",
+                        default="cuda")
+    parser.add_argument("--width",
+                        help="width of the cropped area, in pixels (default: %(default)ipx)",
+                        type=int,
+                        default=1790)
+    parser.add_argument("--height",
+                        help="Height of the cropped area, in pixels (default: %(default)ipx)",
+                        type=int,
+                        default=1660)
+
+    args = parser.parse_args()
     
-    if "--verbose" in sys.argv or "-v" in sys.argv:
-        __DEBUG__ = True
+    __DEBUG__ = args.verbose
 
-    _log("Initializing QT.")
-    QtWidgets.QApplication(sys.argv)
+    INNER_WIDTH = args.width
+    INNER_HEIGHT = args.height
 
-    main(sys.argv[1], True if "--plot" in sys.argv or "-p" in sys.argv else False)
+    if args.plot:
+        _log("Initializing QT.")
+        QtWidgets.QApplication(sys.argv)
+
+    main(args)
